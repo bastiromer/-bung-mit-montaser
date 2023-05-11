afc06db8b2f95a36442c2bdfbf4f47151ee94665
Basti <bastian.romer@web.de>
2023-05-11 12:37:52 +0200

    setup test

--- src/main/scala/test/main.scala
+++ src/main/scala/test/main.scala
@@ -5,10 +5,10 @@ import test.Messages
 object main:
 
   @main def run: Unit =
-    println(welcomeMessage())
     val msg = new Messages
+    println(welcomeMessage())
     print(msg.printHallo())
     print(msg.printTestText())
 
   def welcomeMessage(): String =
-    "\u001B[31mWelcome to Test Repo!!\u001B[0m\n"
+    "Welcome to Test Repo!!"
